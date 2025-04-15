d5dabc3d3be2a9786b0edd714e580ef9af43779d
nitram26 <carlosmartin1993@gmail.com>
2025-04-15 08:57:46 -0500

    2504150857

--- src/views/front/VueView.vue
+++ src/views/front/VueView.vue
@@ -228,80 +228,84 @@
       <ol>
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Contador</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/Contador.html" class="a" target="_blank">
+                <h4>Contador </h4>
              </a>
               <ol>
-                <li></li>
-                <li></li>
+                <li>agrega y descuenta a un número</li>
+                <li>Se puede agregar muchas otras opciones</li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/contador.png " alt="contador " class="img1">
+              <p><br></p>
+            </details>
+          </li>
+    <!-- ****************************************** -->
+    <li class="li">
+            <details class="select">
+              <summary>Reactividad</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/ReactividadBasica.html" class="a" target="_blank">
+                <h4>Básica</h4>
+             </a>
+              <ol>
+                <li>Unido al contador</li>
+                <li>Cambia de color pasando 1000</li>
+                <li>Inicia en 500, pero se puede cambiar fácilmente </li>
+              </ol>
+              <img src="../../assets/image/front/vue3/reactividad.png " alt="reactividad " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
+              <summary>V_FOR</summary>
               <a href="" class="a" target="_blank">
-                <h4></h4>
+                <h4>Recorre y muestra Array y Objetos</h4>
              </a>
               <ol>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>DEL 1 AL 10 separado por guion o espacio</li>
+                <li>Iterar las letras de una palabra</li>
+                <li>Iterar un array de números o letras</li>
+                <li>Iterar un objeto</li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/v-for.png " alt="v-for " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Practica Uno</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/PracticaUno.html" class="a" target="_blank">
+                <h4>Practica Básica</h4>
              </a>
               <ol>
-                <li></li>
-                <li></li>
+                <li>Mostrar saldo</li>
+                <li>Botones de depósito y retiro</li>
+                <li>si esta en 0 que no pueda retirar //usar un alert</li>
+                <li>cantidad de movimientos</li>
+                <li>cantidad de movimientos</li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/practicaUno.png " alt="practicaUno " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>V-FOR</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/OpcionVfor.html" class="a" target="_blank">
+                <h4>v-for</h4>
              </a>
               <ol>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
-              <p><br></p>
-            </details>
-          </li>
-    <!-- ****************************************** -->
-        <li class="li">
-            <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
-             </a>
-              <ol>
-                <li></li>
-                <li></li>
+                <li>Hacer un card y tener un objeto de preguntas</li>
+                <li>Que desaparezcan después de 3 segundos las respuestas</li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/OpcionVfor.png " alt="OpcionVfor " class="img1">
               <p><br></p>
             </details>
           </li>
@@ -313,60 +317,64 @@
       <ol>
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Rutas</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/Enrutado.html" class="a" target="_blank">
+                <h4>USANDO VUETIFY</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/ruta1.png " alt="ruta1 " class="img1">
+              <img src="../../assets/image/front/vue3/ruta2.png " alt="ruta2 " class="img1">
+              <img src="../../assets/image/front/vue3/ruta3.png " alt="ruta3 " class="img1">
+              <img src="../../assets/image/front/vue3/ruta4.png " alt="ruta4 " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>LAYOUTS & COMPOSABLES</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/layoutsComposables.html" class="a" target="_blank">
+                <h4>LAYOUTS</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Muy util para no repetir Código</li>
                 <li></li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/leyautDeclarativa01.png " alt="leyautDeclarativa01 " class="img1">
+              <img src="../../assets/image/front/vue3/leyautDeclarativa02.png " alt="leyautDeclarativa02 " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Animaciones e Iconos </summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/Animacion&Icono.html" class="a" target="_blank">
+                <h4>Proceso</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/iconos01.png " alt=" " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Router</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/routerVuetify.html" class="a" target="_blank">
+                <h4>ROUTER CON VUETIFY</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
@@ -409,12 +417,12 @@
       <ol>
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Props</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/propsVuetres.html" class="a" target="_blank">
+                <h4>Componentes Padre Hijo</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Analizar el proceso</li>
                 <li></li>
                 <li></li>
               </ol>
@@ -425,64 +433,64 @@
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Emits</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/EmitVue.html" class="a" target="_blank">
+                <h4>Emits y Props</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/emits1.png " alt="emits1 " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Props Array</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/PrpsArray.html" class="a" target="_blank">
+                <h4>Props Arrays</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/propsArray.png " alt=" propsArray" class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Injection</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/Injection.html" class="a" target="_blank">
+                <h4>Reemplaza a los props y emits</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Mucho más fácil</li>
                 <li></li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/injection.png " alt="injection " class="img1">
               <p><br></p>
             </details>
           </li>
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>PlaceHolder</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/ServicePlaceholder.html" class="a" target="_blank">
+                <h4>Uso de Placeholder</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
+              <img src="../../assets/image/front/vue3/placeholder.PNG " alt=" placeholder" class="img1">
               <p><br></p>
             </details>
           </li>
@@ -494,12 +502,12 @@
       <ol>
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>login Firebase</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/LoginFirebase.html" class="a" target="_blank">
+                <h4>Autenticar</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
@@ -510,12 +518,30 @@
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Pinia</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/pinia.html" class="a" target="_blank">
+                <h4>Pinia</h4>
              </a>
               <ol>
+                <li>Revisar</li>
                 <li></li>
+                <li></li>
+              </ol>
+              <img src="../../assets/image/front/vue3/piniaMain.png " alt="piniaMain " class="img1">
+              <img src="../../assets/image/front/vue3/piniaCounter.png " alt=" piniaCounter" class="img1">
+              <img src="../../assets/image/front/vue3/piniaComponent.png " alt="piniaComponent " class="img1">
+              <p><br></p>
+            </details>
+          </li>
+    <!-- ****************************************** -->
+        <li class="li">
+            <details class="select">
+              <summary>PWA</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/pwa.html" class="a" target="_blank">
+                <h4>PWA</h4>
+             </a>
+              <ol>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
@@ -526,12 +552,12 @@
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>il18n</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/pwa.html" class="a" target="_blank">
+                <h4>il18n</h4>
              </a>
               <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>
@@ -542,28 +568,12 @@
     <!-- ****************************************** -->
         <li class="li">
             <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
+              <summary>Plugins</summary>
+              <a href="http://127.0.0.1:5502/src/CodigosHtml/front/vue3/plugins.html" class="a" target="_blank">
+                <h4>Plugins</h4>
              </a>
               <ol>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ol>
-              <img src="../../assets/image/front/vue3 " alt=" " class="img1">
-              <p><br></p>
-            </details>
-          </li>
-    <!-- ****************************************** -->
-        <li class="li">
-            <details class="select">
-              <summary></summary>
-              <a href="" class="a" target="_blank">
-                <h4></h4>
-             </a>
-              <ol>
-                <li></li>
+                <li>Revisar</li>
                 <li></li>
                 <li></li>
               </ol>

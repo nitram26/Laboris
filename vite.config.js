e471bbdcdcf21b19e7254912bb441191efc2bd2d
nitram26 <carlosmartin1993@gmail.com>
2025-12-22 07:41:19 -0500

    2512220741

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify' 
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
-    // Add any other plugins you need here
+    // otros plugins si los necesitas
   ],
+  server: {
+    port: 5503,        // <--- aquí defines el puerto del servidor de desarrollo
+    open: true,        // (opcional) abre automáticamente el navegador
+    host: true         // (opcional) permite acceso desde la red local
+  }
 })
